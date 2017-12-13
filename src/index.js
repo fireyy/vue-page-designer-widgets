7c310eb9484b36dacbedc747349f9458bfb0557c
fireyy <fireyy@gmail.com>
2017-12-13 12:04:55 +0800

    fix: remove container

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import braidPic from './pic'
 import braidPicStyle from './pic/style.vue'
 import braidTxt from './txt'
 import braidTxtStyle from './txt/style.vue'
-import braidContainer from './container'
-import braidContainerStyle from './container/style.vue'
 
 export const widgets = {
   braidBg,
   braidPic,
-  braidTxt,
-  braidContainer
+  braidTxt
 }
 
 export const widgetStyle = {
   braidBgStyle,
   braidPicStyle,
-  braidTxtStyle,
-  braidContainerStyle
+  braidTxtStyle
 }
