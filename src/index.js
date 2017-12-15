7e31d768574c8cd3a26cec5e2898059778357cef
fireyy <fireyy@gmail.com>
2017-12-15 10:17:43 +0800

    fix: import default file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import braidBg from './bg'
+import braidBg from './bg/index.vue'
 import braidBgStyle from './bg/style.vue'
-import braidPic from './pic'
+import braidPic from './pic/index.vue'
 import braidPicStyle from './pic/style.vue'
-import braidTxt from './txt'
+import braidTxt from './txt/index.vue'
 import braidTxtStyle from './txt/style.vue'
 
 export const widgets = {
