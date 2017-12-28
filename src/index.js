767e73be551d7a530bb3b4fe76a0affffdb2d71e
fireyy <fireyy@gmail.com>
2017-12-28 10:27:29 +0800

    feat: revert container

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import braidBg from './bg/index.vue'
 import braidPic from './pic/index.vue'
 import braidTxt from './txt/index.vue'
+import braidContainer from './container/index.vue'
 
 export default {
   braidBg,
   braidPic,
-  braidTxt
+  braidTxt,
+  braidContainer
 }
